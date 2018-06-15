0475eff5905e6b94b8bde8c6feb65ff73aaf731d
Alex Sánchez Pla <asanchez@ub.edu>
2018-06-15 15:00:36 +0200

    Uploaded all datasets

--- lab-2_Linear_Models/datasets/GetData.R
+++ lab-2_Linear_Models/datasets/GetData.R
@@ -17,7 +17,7 @@ installifnot("GEOquery")
 #---------------------------------------------------------------------------------------------
 
 fileList<- dir()
-datDirs <- grep("dataset", fleList)
+datDirs <- grep("dataset", fileList)
 dataDirs <- fileList[datDirs]
 GEOSETS <- c("GSE18198", "GSE1561", "GSE100924", "GSE68580")
 
@@ -25,7 +25,7 @@ GEOSETS <- c("GSE18198", "GSE1561", "GSE100924", "GSE68580")
 require(GEOquery)
 
 ###########################################
-### Dataset 1
+### Dataset 1 (GSE18198)
 ###########################################
 
 i <- 1
@@ -48,6 +48,79 @@ dir2Save <- paste(dataDirs[i], GEOSETS[i], sep="/")
 write.csv(targets,file=paste(dir2Save, "txt", sep=".") )
 save(eset, expresMat, targets, file=paste(dir2Save, "Rda", sep="."))
 
+###########################################
+### Dataset 2 (GSE1561)
+###########################################
+
+i <- 2
+rawData <- getGEO(GEOSETS[i])
+class(rawData); names(rawData); class(rawData[[1]])
+eset <-rawData[[1]]
+head(pData(eset))
+
+#TARGETS
+colnames(pData(eset))
+targets <-pData(eset)[,c("title","type")] 
+targets
+
+# Expression Matrix
+expresMat <- exprs(eset)
+colnames(expresMat) <- targets$title
+
+dir2Save <- paste(dataDirs[i], GEOSETS[i], sep="/")
+
+write.csv(targets,file=paste(dir2Save, "txt", sep=".") )
+save(eset, expresMat, targets, file=paste(dir2Save, "Rda", sep="."))
+
+###########################################
+### Dataset 4 (GSE100924)
+###########################################
+
+i <- 3
+rawData <- getGEO(GEOSETS[i])
+class(rawData); names(rawData); class(rawData[[1]])
+eset <-rawData[[1]]
+dim(exprs(eset))
+
+#TARGETS
+colnames(pData(eset))
+head(pData(eset))
+targets <-pData(eset)[,c("cold exposure:ch1",  "genotype:ch1")] 
+targets
+
+# Expression Matrix
+expresMat <- exprs(eset)
+# colnames(expresMat) <- targets$title
+
+dir2Save <- paste(dataDirs[i], GEOSETS[i], sep="/")
+
+write.csv(targets,file=paste(dir2Save, "txt", sep=".") )
+save(eset, expresMat, targets, file=paste(dir2Save, "Rda", sep="."))
+
+###########################################
+### Dataset 5 (GSE68580)
+###########################################
+
+i <- 4
+rawData <- getGEO(GEOSETS[i])
+class(rawData); names(rawData); class(rawData[[1]])
+eset <-rawData[[1]]
+dim(exprs(eset))
+
+#TARGETS
+colnames(pData(eset))
+head(pData(eset))
+targets <-pData(eset)[,c("title","geo_accession")] 
+targets
+
+# Expression Matrix
+expresMat <- exprs(eset)
+# colnames(expresMat) <- targets$title
+
+dir2Save <- paste(dataDirs[i], GEOSETS[i], sep="/")
+
+write.csv(targets,file=paste(dir2Save, "txt", sep=".") )
+save(eset, expresMat, targets, file=paste(dir2Save, "Rda", sep="."))
 
 
 
