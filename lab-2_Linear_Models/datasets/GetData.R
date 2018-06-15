04e52eda931c70114ceee52f4436ed045aeb5ad0
alexsanchezpla <asanchez@ub.edu>
2018-06-15 08:30:28 +0200

    Prepare datasets for linear models exercises

---
+++ lab-2_Linear_Models/datasets/GetData.R
@@ -0,0 +1,55 @@
+#---------------------------------------------------------------------------------------------
+###INSTALLATION OF PACKAGES NEEDED
+#---------------------------------------------------------------------------------------------
+
+installifnot <- function (pkg){
+  if (!require(pkg, character.only=T)){
+  source("http://bioconductor.org/biocLite.R")
+  biocLite(pkg)
+}else{
+  require(pkg, character.only=T)
+  }
+}
+installifnot("GEOquery")
+
+#---------------------------------------------------------------------------------------------
+###LOAD DATA: TARGETS AND CEL FILES. 
+#---------------------------------------------------------------------------------------------
+
+fileList<- dir()
+datDirs <- grep("dataset", fleList)
+dataDirs <- fileList[datDirs]
+GEOSETS <- c("GSE18198", "GSE1561", "GSE100924", "GSE68580")
+
+# Get data from the web
+require(GEOquery)
+
+###########################################
+### Dataset 1
+###########################################
+
+i <- 1
+rawData <- getGEO(GEOSETS[i])
+class(rawData); names(rawData); class(rawData[[1]])
+eset <-rawData[[1]]
+pData(eset)
+
+#TARGETS
+colnames(pData(eset))
+targets <-pData(eset)[,c("title", "cell line:ch1")] 
+targets
+
+# Expression Matrix
+expresMat <- exprs(eset)
+colnames(expresMat) <- targets$title
+
+dir2Save <- paste(dataDirs[i], GEOSETS[i], sep="/")
+
+write.csv(targets,file=paste(dir2Save, "txt", sep=".") )
+save(eset, expresMat, targets, file=paste(dir2Save, "Rda", sep="."))
+
+
+
+
+
+
