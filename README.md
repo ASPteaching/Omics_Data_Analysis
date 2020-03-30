a371398d6fc545d43b8d82e6290ba829f57ddec3
alexsanchezpla <asanchez@ub.edu>
2020-03-30 17:25:38 +0200

    Cleaned some, not all obsolete files and organized materials

--- README.md
+++ README.md
@@ -1,40 +1,21 @@
 # Omics Techniques
 
-This repository is intended to support some sessions in the "Omics Techniques" course in the Bioinformatics degree. Overall this block deals with distinct aspects of quality checking in Omics studies.  
+This repository is intended to support some sessions in the "Omics Techniques" course in the UB-UPC-UAB-UPF interuniversitary Bioinformatics Degree. 
+
+Overall this block deals with distinct aspects of omics data analysis, mostly focused in gene expression data analysis.
+
+0. **Get familiar with Bioconductor**
+Bioconductor is a set of R packages that provides state-of-the art methods and tools for omics data analysis. This first session introduces the general ideas and shows how to work with it using an OOP aproach to Bioconductor classes.
 
 1. **Experimental design**. Instead of looking for problems in the data try to avoid them from the begining of the study. An appropriate experimental design can help researchers get the most from their data and alo avoid common error such as confusion between effects.
+After reviewing some key ideas of experimental design the practgical exercises focus on recognizing types and components of experimental designs in practical situations.
 
-2. **Batch effect detection and removal**. One of most common problems in omics techniques is that, conciously or unadvertedly, samples are often generated in batches. Experimental design may be used to control known batches. There may be, however,  unknown ones which must be detected and whose effects must be removed.
+2. **Linear models**
+Linear models provide the natural framework for analyzing data obtained from "designed experiments". This sessions reviews basic ideas of linear modelling and introduces the linear model framework for microarray data analysis. If time permits it will be extended to GLM for discrete (counts from NGS) data analysis.
 
-3. **Quality control and quality checking in sequencing** Sequencing goes through different  phases and problems may appear in many of these steps. It is important to know how to detect the effects of this problems and what to do to mitigate its effects.
+3. **Batch effect detection and removal**. One of most common problems in omics techniques is that, conciously or unadvertedly, samples are often generated in batches. Experimental design may be used to control known batches. There may be, however,  unknown ones which must be detected and whose effects must be removed. We will review PCA and see how to use it to detect batch effects. We will also see examples of how to use a linear model to account for batch effects if batch factors are known, and which techniques are there available to deal with unknown factors.
 
-## Experimental design
-
-There are two slides sets
-
-- The first set (Session 1) is an introduction to experimental design and experimental design principles
-- The second set (Session 2) contains an introduction to linear models and its relation with experimental design
-
-The lab session will do three things
-
-- First, an introduction to bioconductor and a quick review of microarray technology will be perfomed.
-- Next, a basic workflow for microarray analysis will be introduced.
-- The "natural way" to reflect experimental design in data analysis is using linear models. This can be done in omics data analysis with the methodology introduced by the `limma` or `edgeR` packages.
-- Additional resources for a omics data analysis and reporting will be introduced and used such as.
-	-  Gene Expression Omnibus Database (GEO)
-	-  R markdown
-	-  github
-	
-- In this lab we will explore how linear modelling can be used to
-
-	- Deploy a certain experimental design in an omics study
-	- Analyze data under this design.
-	
-- This illustration will be done using the most classic omics data, expression microarrays. Some supporting materials about microarrays 
-can be found in the "lab-1-Microarrays" folder.
-
-	- Additional information can be found in [https://github.com/alexsanchezpla/StatisticalAnalysisOfMicroarrayData](https://github.com/alexsanchezpla/StatisticalAnalysisOfMicroarrayData)
-
+4. **Quality control and quality checking in sequencing** Sequencing goes through different  phases and problems may appear in many of these steps. It is important to know how to detect the effects of this problems and what to do to mitigate its effects.
 
 		
 		
---
+++ lab-0_Bioconductor_classes/Practical-Bioconductor-Classes.R
@@ -0,0 +1,137 @@
+## ----include=FALSE-------------------------------------------------------
+require(knitr)
+opts_chunk$set(
+concordance=FALSE, echo=TRUE, cache=TRUE, warning=FALSE, error=FALSE, message=FALSE)
+
+
+## ----simulateData--------------------------------------------------------
+expressionValues <- matrix (rnorm (300), nrow=30)
+colnames(expressionValues) <- paste0("sample",1:10)
+head(expressionValues)
+
+
+## ----simulateCovariates--------------------------------------------------
+targets <- data.frame(sampleNames = paste0("sample",1:10), group=c(paste0("CTL",1:5),paste0("TR",1:5)),age = rpois(10, 30), sex=as.factor(sample(c("Male", "Female"),10,replace=TRUE)))
+head(targets, n=10)
+
+
+## ----simulateGeneInfo----------------------------------------------------
+myGenes <-  paste0("gene",1:30)
+
+
+## ----simulateInfo--------------------------------------------------------
+myInfo=list(myName="Alex Sanchez", myLab="Bioinformatics Lab", 
+          myContact="alex@somemail.com", myTitle="Practical Exercise on ExpressionSets")
+show(myInfo)
+
+
+## ------------------------------------------------------------------------
+pcs <- prcomp(expressionValues)
+names(pcs)
+barplot(pcs$sdev)
+plot(pcs$rotation[,1], pcs$rotation[,2], col=targets$group, main="Representation of first two principal components")
+text(pcs$rotation[,1], pcs$rotation[,2],targets$sex)
+
+
+## ------------------------------------------------------------------------
+variab <- apply(expressionValues, 1, sd)
+orderedGenes <- myGenes[order(variab, decreasing=TRUE)]
+head(variab[order(variab, decreasing=TRUE)])
+head(orderedGenes)
+
+
+## ----subsetExpressions---------------------------------------------------
+newExpress<- expressionValues[,-9]
+newTargets <- targets[-9,]
+wrongNewTargets <- targets [-10,]
+
+
+## ----loadPackage---------------------------------------------------------
+require(Biobase)
+
+
+## ----creaExpressionSet1--------------------------------------------------
+myEset <- ExpressionSet(expressionValues)
+class(myEset)
+show(myEset)
+
+
+## ----AnnotatedDataFrame2-------------------------------------------------
+columnDesc <-  data.frame(labelDescription= c("Sample Names", "Treatment/Control", "Age at disease onset", "Sex of patient (Male/Female"))
+myAnnotDF <- new("AnnotatedDataFrame", data=targets, varMetadata= columnDesc)
+show(myAnnotDF)
+
+
+## ------------------------------------------------------------------------
+phenoData(myEset) <- myAnnotDF
+
+
+## ----eval=FALSE----------------------------------------------------------
+## # myEset <- ExpressionSet(assayData=expressionValues, phenoData=myAnnotDF)
+## # Error in validObject(.Object) :
+## #   invalid class ExpressionSet object: 1: sampleNames differ between assayData and phenoData
+## # invalid class ExpressionSet object: 2: sampleNames differ between phenoData and protocolData
+
+
+## ------------------------------------------------------------------------
+rownames(pData(myAnnotDF))<-pData(myAnnotDF)$sampleNames
+myEset <- ExpressionSet(assayData=expressionValues, phenoData=myAnnotDF)
+show(myEset)
+
+
+## ------------------------------------------------------------------------
+myEset <- ExpressionSet(assayData=expressionValues, 
+                        phenoData=myAnnotDF, 
+                        featureNames =myGenes)
+# show(myEset)
+
+
+## ----label=MIAME---------------------------------------------------------
+myDesc <- new("MIAME", name= myInfo[["myName"]],
+            lab= myInfo[["myLab"]],
+            contact= myInfo[["myContact"]] ,
+            title=myInfo[["myTitle"]])
+print(myDesc)
+
+
+## ------------------------------------------------------------------------
+myEset <- ExpressionSet(assayData=expressionValues, 
+                        phenoData=myAnnotDF,
+                        fetureNames =myGenes,
+                        experimentData = myDesc)
+# show(myEset)
+
+
+## ----usingExpressionSets-------------------------------------------------
+dim(exprs(myEset))
+class(phenoData(myEset))
+class(pData(phenoData(myEset)))
+head(pData(phenoData(myEset)))
+head(pData(myEset))
+
+
+## ------------------------------------------------------------------------
+smallEset <- myEset[1:15,c(1:3,6:8)]
+dim(exprs(smallEset))
+dim(pData(smallEset))
+head(pData(smallEset))
+all(colnames(exprs(smallEset))==rownames(pData(smallEset)))
+
+
+## ------------------------------------------------------------------------
+youngEset <- myEset[,pData(myEset)$age<30]
+dim(exprs(youngEset))
+head(pData(youngEset))
+
+
+## ------------------------------------------------------------------------
+if (!require(GEOquery)) {
+  BiocManager::install("GEOquery")
+}
+require(GEOquery)
+gse <- getGEO("GSE507")
+class(gse)
+names(gse)
+gse[[1]]
+esetFromGEO <- gse[[1]]
+
